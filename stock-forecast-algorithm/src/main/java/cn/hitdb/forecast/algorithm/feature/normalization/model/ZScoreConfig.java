38c6adc013048d7e899f332e6c8b5e4d50bd20c5
weihuang <weihuanghit@163.com>
2018-02-19 02:17:00 +0800

    normalization

---
+++ stock-forecast-algorithm/src/main/java/cn/hitdb/forecast/algorithm/feature/normalization/model/ZScoreConfig.java
@@ -0,0 +1,6 @@
+package cn.hitdb.forecast.algorithm.feature.normalization.model;
+
+import cn.hitdb.core.model.ml.MLConfigration;
+
+public class ZScoreConfig extends MLConfigration{
+}
---
+++ stock-forecast-algorithm/src/main/java/cn/hitdb/forecast/algorithm/feature/normalization/train/ZScoreAlgorithm.java
@@ -0,0 +1,4 @@
+package cn.hitdb.forecast.algorithm.feature.normalization.train;
+
+public class ZScoreAlgorithm {
+}
