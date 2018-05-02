4672604259c03826471bb3c1f3d354a277a2c19f
weihuang <weihuanghit@163.com>
2018-05-02 08:28:02 +0800

    modify .rdconfig

---
+++ stock-forecast-core/src/test/java/TestUnsafeSerialization.java
@@ -0,0 +1,12 @@
+package PACKAGE_NAME;
+
+/**
+ * <p>Description: </p>
+ * <p>Company: Harbin Institute of Technology</p>
+ *
+ * @author weihuang
+ * @date 2018/3/4
+ * @time 上午1:11
+ */
+public class TestUnsafeSerialization {
+}
